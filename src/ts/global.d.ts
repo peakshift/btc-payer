05aa249bf9f77b5b8efc3b7db521415584581075
Jonathan Palma <jonathanpalma.me@gmail.com>
2021-12-01 16:20:43 -0600

    fix: fix global declaration file

--- src/ts/global.d.ts
+++ src/ts/global.d.ts
@@ -43,32 +43,32 @@ interface IPaymentRequestDetails {
 }
 
 interface IDecodedInvoice {
-//  chain_address?: string;
-//  cltv_delta: number;
-//  created_at: string;
+  //  chain_address?: string;
+  //  cltv_delta: number;
+  //  created_at: string;
   description?: string;
   description_hash?: string;
-//  destination: string;
+  //  destination: string;
   expires_at: string;
-//  features: {
-//    bit: number;
-//    is_required: boolean;
-//    type: string;
-//  }[];
-//  id: string;
+  //  features: {
+  //    bit: number;
+  //    is_required: boolean;
+  //    type: string;
+  //  }[];
+  //  id: string;
   is_expired: boolean;
-//  mtokens: string;
+  //  mtokens: string;
   network: string;
-//  payment?: string;
-//  routes?: {
-//    base_fee_mtokens?: string;
-//    channel?: string;
-//    cltv_delta?: number;
-//    fee_rate?: number;
-//    public_key: string;
-//  };
-//  safe_tokens: number;
-//  tokens: number;
+  //  payment?: string;
+  //  routes?: {
+  //    base_fee_mtokens?: string;
+  //    channel?: string;
+  //    cltv_delta?: number;
+  //    fee_rate?: number;
+  //    public_key: string;
+  //  };
+  //  safe_tokens: number;
+  //  tokens: number;
 }
 
 interface IConnectorConfig {
@@ -93,6 +93,6 @@ interface IBTCPayServer {
   getAccounts(): Array<IAccount>;
   getBalance(): Promise<string>;
   getInvoices(): Promise<Array<IPaymentRequestDetails>>;
-  makeInvoice(): Promise<IInvoice>;
+  makeInvoice(): Promise<IDecodedInvoice>;
   sendPayment(): Promise<boolean>;
 }
