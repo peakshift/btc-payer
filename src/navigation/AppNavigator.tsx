6f14b46fce5a4b34fa269a75ce904ca831cdb5e9
Jonathan Palma <jonathanpalma.me@gmail.com>
2022-01-29 16:16:21 -0600

    feat: add bottom tab navigation
    resolve #3

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import {createStackNavigator} from '@react-navigation/stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import HomeScreen from '@app/scenes/HomeScreen';
-import InvoiceScreen from '@app/scenes/InvoiceScreen';
 import ReceivePaymentScreen from '@app/scenes/ReceivePaymentScreen';
 import SendPaymentScreen from '@app/scenes/SendPaymentScreen';
 
@@ -22,23 +21,21 @@ export const routes = Object.freeze<{
   [key: string]: keyof AppNavigatorParams;
 }>({
   HOME: 'HomeScreen',
-  INVOICE: 'InvoiceScreen',
   RECEIVE_PAYMENT: 'ReceivePaymentScreen',
   SEND_PAYMENT: 'SendPaymentScreen',
 });
 
-const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 function AppNavigator() {
   return (
-    <Stack.Navigator initialRouteName={routes.HOME}>
-      <Stack.Screen name={routes.HOME} component={HomeScreen} />
-      <Stack.Screen name={routes.INVOICE} component={InvoiceScreen} />
-      <Stack.Screen
+    <Tab.Navigator initialRouteName={routes.HOME}>
+      <Tab.Screen name={routes.HOME} component={HomeScreen} />
+      <Tab.Screen
         name={routes.RECEIVE_PAYMENT}
         component={ReceivePaymentScreen}
       />
-      <Stack.Screen name={routes.SEND_PAYMENT} component={SendPaymentScreen} />
-    </Stack.Navigator>
+      <Tab.Screen name={routes.SEND_PAYMENT} component={SendPaymentScreen} />
+    </Tab.Navigator>
   );
 }
 
